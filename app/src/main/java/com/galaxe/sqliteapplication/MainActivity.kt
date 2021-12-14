7676365fb3706d0baa82453cc7f349de40943897
CRenauro <crenauro@galaxe.com>
2021-12-14 09:52:48 -0500

    Initial commit

---
+++ app/src/main/java/com/galaxe/sqliteapplication/MainActivity.kt
@@ -0,0 +1,95 @@
+package com.galaxe.sqliteapplication
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var enterName : EditText
+    lateinit var enterAge : EditText
+
+    lateinit var Name : TextView
+    lateinit var Age : TextView
+
+    @SuppressLint("Range")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var addName  = findViewById<Button>(R.id.addName)
+        var printName = findViewById<Button>(R.id.printName)
+
+        enterName = findViewById(R.id.enterName)
+        enterAge = findViewById<EditText>(R.id.enterAge)
+
+        Name = findViewById(R.id.Name)
+        Age = findViewById(R.id.Age)
+
+
+        addName.setOnClickListener{
+
+            // below we have created
+            // a new DBHelper class,
+            // and passed context to it
+            val db = DBHelper(this, null)
+
+            // creating variables for values
+            // in name and age edit texts
+            val name = enterName.text.toString()
+            val age = enterAge.text.toString()
+
+            // calling method to add
+            // name to our database
+            db.addName(name, age)
+
+            // Toast to message on the screen
+            Toast.makeText(this, name + " added to database", Toast.LENGTH_LONG).show()
+
+            // at last, clearing edit texts
+            enterName.text.clear()
+            enterAge.text.clear()
+        }
+
+        // below code is to add on click
+        // listener to our print name button
+        printName.setOnClickListener{
+
+            // creating a DBHelper class
+            // and passing context to it
+            val db = DBHelper(this, null)
+
+            // below is the variable for cursor
+            // we have called method to get
+            // all names from our database
+            // and add to name text view
+            val cursor = db.getName()
+
+            // moving the cursor to first position and
+            // appending value in the text view
+            cursor!!.moveToFirst()
+            // Name.append(cursor.getString(cursor.getColumnIndex(DBHelper.NAME_COl)) + "\n")
+            //Age.append(cursor.getString(cursor.getColumnIndex(DBHelper.AGE_COL)) + "\n")
+            var n = cursor.getString(cursor.getColumnIndex(DBHelper.NAME_COl)) + "\n"
+            var a =cursor.getString(cursor.getColumnIndex(DBHelper.AGE_COL)) + "\n"
+
+
+            Toast.makeText(this,"Values are $n $a",Toast.LENGTH_SHORT).show()
+
+            while(cursor.moveToNext()){
+                //  Name.append(cursor.getString(cursor.getColumnIndex(DBHelper.NAME_COl)) + "\n")
+                // Age.append(cursor.getString(cursor.getColumnIndex(DBHelper.AGE_COL)) + "\n")
+
+                Toast.makeText(this,"Values are $n $a",Toast.LENGTH_SHORT).show()
+
+
+            }
+
+            cursor.close()
+        }
+    }
+}
